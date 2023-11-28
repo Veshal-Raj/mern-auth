eb072446465ec09e630ce4d469c12b946893aee5
Veshal Raj <veshalraj1307@gmail.com>
2023-11-28 13:50:29 +0530

    create sign in api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,9 @@
 import User from "../models/UserModal.js";
+import { errorHandler } from "../utils/error.js";
+import dotenv from "dotenv";
+dotenv.config();
 import bcryptjs from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   // console.log(req.body)
@@ -9,6 +13,25 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successflly" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(403, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000)
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
