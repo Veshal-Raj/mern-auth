d6b18f21366436c1c09eb09b98281ff9b85dfe0a
Veshal Raj <veshalraj1307@gmail.com>
2023-12-01 12:00:39 +0530

    Change user role functionality completed

--- api/routes/adminRoute.js
+++ api/routes/adminRoute.js
@@ -3,6 +3,7 @@ import express from "express";
 import getAllUsers from '../controllers/Admin/getAllUsers.js'
 import { verifyToken } from "../utils/verifyUser.js";
 import toggleIsVerified from "../controllers/Admin/toggleIsVerified.js";
+import changeRole from "../controllers/Admin/changeRole.js";
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ const router = express.Router();
 // });
 router.get('/getAllUsers', verifyToken, getAllUsers)
 router.put('/blockUser/:id', verifyToken, toggleIsVerified)
+router.put('/changeRole/:id', verifyToken, changeRole)
 // router.delete('/delete/:id',verifyToken, deleteUser)
 
 
--- client/src/components/UserTable.jsx
+++ client/src/components/UserTable.jsx
@@ -1,54 +1,78 @@
 import { useEffect, useState } from "react";
+import MakeAdminButton from "./admin/MakeAdminButton";
 
 export const UserTable = () => {
-    const [userData, setUserData] = useState([])
-    // const [blockingUserId, setBlockingUserId] = useState(null);
+  const [userData, setUserData] = useState([]);
 
-
-    // console.log(userData)
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await fetch('/api/admin/getAllUsers');
-                const data = await response.json()
-                setUserData(data)
-            } catch (error) {
-                console.error('Error fetching data: ',error);
-            }
-        }
-
-        fetchUserData()
-    },[])
-    console.log(userData)
-
-    const editUser = () =>{
-      console.log('edit user is clicked')
-    }
-
-    const blockUser = async (userId, isActive) => {
-      // console.log('user id', userId )
-      // console.log('isActive ',isActive)
-     try {
-      const response = await fetch(`/api/admin/blockUser/${userId}`, {
-        method: 'PUT' ,
-        headers:{
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ isActive: isActive}),
-      })
-      // console.log(response)
-      if (response.ok) {
-        setUserData(prevData => (
-          prevData.map(user => (user._id === userId ? {...user, isActive: !isActive} : user))
-        ))
-      } else {
-        console.error('Error updating user isVerified status');
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch("/api/admin/getAllUsers");
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
       }
-     } catch (error) {
-      console.error('Error updating user isVerified status: ', error);
-     }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const editUser = () => {
+    console.log("edit user is clicked");
+  };
+
+  const blockUser = async (userId, isActive) => {
+    try {
+      const response = await fetch(`/api/admin/blockUser/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ isActive: isActive }),
+      });
+
+      if (response.ok) {
+        setUserData((prevData) =>
+          prevData.map((user) =>
+            user._id === userId ? { ...user, isActive: !isActive } : user
+          )
+        );
+      } else {
+        console.error("Error updating user isVerified status");
+      }
+    } catch (error) {
+      console.error("Error updating user isVerified status: ", error);
     }
-    // console.log(userData)
+  };
+
+  const changeRole = async (userId, isAdmin) => {
+    console.log(isAdmin);
+    try {
+      const response = await fetch(`/api/admin/changeRole/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ isAdmin }),
+      });
+      // console.log(response.body)
+      if (response.ok) {
+        const responseData = await response.json();
+      const updatedRole = responseData.data;
+        setUserData((prevData) =>
+        prevData.map((user) =>
+          user._id === userId ? { ...user, role: updatedRole } : user
+        )
+      );
+      } else {
+        console.error("Error updating user role");
+      }
+    } catch (error) {
+      console.error("Error updating user role: ", error);
+    }
+  };
+
   return (
     <div className="mx-auto max-w-2xl overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800">
@@ -62,40 +86,44 @@ export const UserTable = () => {
           </tr>
         </thead>
         <tbody>
-  {/* Map through userData and render table rows dynamically */}
-  {userData
-    .filter(user => user.role !== 'Admin') // Filter out Admin users
-    .map(user => (
-      <tr key={user._id} className="border-b hover:bg-gray-50 dark:border-gray-700 dark:hover:bg-gray-600">
-        <td className="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
-          {user.username}
-        </td>
-        <td className="px-6 py-3">{user.role}</td>
-        <td className="px-6 py-3 uppercase">{user.status}</td>
-        <td className="px-6 py-3 text-right">
-          <span onClick={editUser} id="editUser" className="text-blue-600 hover:underline hover:cursor-pointer">
-            Edit
-          </span>
-          <span className="mx-2 text-gray-500 dark:text-gray-400">|</span>
-          <span onClick={() => blockUser(user._id, user.isActive)} id="blockUser" className="text-red-600 hover:underline hover:cursor-pointer">
-              {user.isActive ? 'Block' : 'UnBlock'}
-          </span>
-          {/* <span onClick={blockUser} id="blockUser" className="text-red-600 hover:underline hover:cursor-pointer">
-            {user.isActive && user.isActive === true ? 'Block':'Unblock'}
-          </span> */}
-        </td>
-        <td className="px-6 py-4">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Make Admin
-          </button>
-        </td>
-      </tr>
-    ))}
-</tbody>
-
+          {/* Map through userData and render table rows dynamically */}
+          {userData.map((user) => (
+            <tr
+              key={user._id}
+              className="border-b hover:bg-gray-50 dark:border-gray-700 dark:hover:bg-gray-600"
+            >
+              <td className="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
+                {user.username}
+              </td>
+              <td className="px-6 py-3">{user.role}</td>
+              <td className="px-6 py-3 uppercase">{user.status}</td>
+              <td className="px-6 py-3 text-right">
+                <span
+                  onClick={editUser}
+                  id="editUser"
+                  className="text-blue-600 hover:underline hover:cursor-pointer"
+                >
+                  Edit
+                </span>
+                <span className="mx-2 text-gray-500 dark:text-gray-400">|</span>
+                <span
+                  onClick={() => blockUser(user._id, user.isActive)}
+                  id="blockUser"
+                  className="text-red-600 hover:underline hover:cursor-pointer"
+                >
+                  {user.isActive ? "Block" : "UnBlock"}
+                </span>
+              </td>
+              <td className="px-6 py-4">
+                <MakeAdminButton
+                  userId={user._id}
+                  isAdmin={user.role}
+                  onChangeRole={changeRole}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
